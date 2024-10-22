c224acd908e7fa1b68bf519bf278371db71b0329
rbeau12 <107488237+rbeau12@users.noreply.github.com>
2024-10-22 17:20:08 +0000

    checkin

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ end
 get("/square/new") do
   erb(:squareTemp)
 end
-get("/square") do
+get("/square/results") do
   number=params.fetch("number").to_f
   number=number**2
   "#{number}"
@@ -19,7 +19,7 @@ end
 get("/square_root/new") do
   erb(:rootTemp)
 end
-get("/square_root") do
+get("/square_root/results") do
   number=params.fetch('number').to_f
   number=number**0.5
   "#{number}"
@@ -29,7 +29,7 @@ get("/random/new") do
   erb(:randTemp)
 end
 
-get("/random") do
+get("/random/results") do
   min=params.fetch("minimum").to_f
   max=params.fetch("maximum").to_f
   returnNum=rand(min..max)
@@ -37,4 +37,17 @@ get("/random") do
 end
 
 get("/payment/new") do
-  erb("paymentTemp")
+  erb(:paymentTemp)
+end
+get("/payment/results") do
+  apr=params.fetch("APR").to_f
+  apr=apr/100
+  apr=apr/12
+  years=params.fetch("years").to_i
+  months=years*12
+  pv=params.fetch("PV").to_f
+  numerator=apr*pv
+  denom= 1-(1+apr)**(-1*months)
+  outcome=(numerator/denom).to_fs(:currency)
+  "#{outcome}"
+end
